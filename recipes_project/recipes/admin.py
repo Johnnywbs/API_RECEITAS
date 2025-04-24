0f73903ea9263451b8b03c868e471b745427be6c
Jonathas dos Santos <89552550+Johnnywbs@users.noreply.github.com>
2025-04-24 00:41:23 -0300

    Delete recipes_project directory

--- recipes_project/recipes/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Ingrediente, Receita, ReceitaIngrediente
-
-admin.site.register(Ingrediente)
-admin.site.register(Receita)
-admin.site.register(ReceitaIngrediente)
